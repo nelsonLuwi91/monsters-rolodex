ec17489d16da3386a2cf69c25f689016e785a314
Nelson Luoie Viñegas Jr <nelsonluoie.vinegasjr@gmail.com>
2020-06-24 13:08:16 +0800

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component"; 
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello there!
         </p>
         <a
           className="App-link"
@@ -16,11 +19,63 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Now!!!
         </a>
       </header>
     </div>
   );
+} */
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+  }
+
+  // handleChange(e) {
+  handleChange = e => {
+    this.setState({searchField: e.target.value});
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    ) 
+    // return(
+    //   <div className="App">
+    //     <input type="search" placeholder="search monsters" onChange={e =>
+    //         this.setState({searchField: e.target.value}) 
+    //     } />
+    //     <CardList monsters={filteredMonsters}>
+    //       /* {
+    //         this.state.monsters.map(monster => (<h1 key={monster.id}>{monster.name}</h1>))
+    //       } */
+    //     </CardList>
+    //   </div>
+    // );
+    return(
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          // handleChange={e => this.setState({searchField: e.target.value})}
+          handleChange={this.handleChange} /> 
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  };
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./card-list-styles.css";
+import { Card } from "../card/card.component";
+
+/* export const CardList = props => {
+  // console.log(props);
+  return(<div className="card-list">{props.children}</div>);
+}; */
+export const CardList = props => (
+  <div className="card-list">
+    {
+      props.monsters.map(monster => (
+        //<h1 key={monster.id}>{monster.name}</h1>
+        <Card key={monster.id} monster={monster} />
+      ))
+    }
+  </div>
+);

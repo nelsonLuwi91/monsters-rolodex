866561a91dc756877bc08444fae8e50441d33536
Nelson Luoie Viñegas Jr <nelsonluoie.vinegasjr@gmail.com>
2020-06-24 13:06:42 +0800

    Updates

---
+++ precache-manifest.54a173936f322bcaf8a2d599fe8090e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457182f4034b8bb7a3509d8e9e0cd617",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e32a22f4878e27037922",
+    "url": "/monsters-rolodex/static/css/main.7336af88.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32a22f4878e27037922",
+    "url": "/monsters-rolodex/static/js/main.231d5a25.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
